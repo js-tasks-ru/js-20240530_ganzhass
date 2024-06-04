92bca63990c1cc1250749a1de60e123219875a59
GanzhaSS <ganzhass@nornik.ru>
2024-06-04 17:48:58 +0300

    Task 2

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,11 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
-
+function sortStrings(arr, param = "asc") {
+  let k;
+  param === "asc" ? (k = 1) : (k = -1);
+  return arr.sort((a, b) => k * a.localeCompare(b, "ru-en"));
 }
+console.log(
+  sortStrings(["Абрикос", "абрикос", "Ёжик", "ёжик", "Яблоко", "яблоко"])
+);
