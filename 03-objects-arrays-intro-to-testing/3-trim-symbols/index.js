5bf60f63aa1c47086c2a9e201ac886aaa9935717
ganzhass <ganzhass@nornik.ru>
2024-06-13 16:31:25 +0300

    3.3 trim

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,13 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
+  if (size < 1 || string === "") return "";
+  if (!size) return string;
+  let currLetter = 0;
+  let newStr = string[0];
+  for (let i = 1; i < string.length; i++) {
+    string[i] === string[i - 1] ? currLetter++ : (currLetter = 0);
+    currLetter < size ? (newStr = newStr + string[i]) : null;
+  }
+  return newStr;
 }
