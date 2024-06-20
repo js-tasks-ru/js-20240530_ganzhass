b8ee6a1b76d22c7262ac337692d368806a38d4e0
ganzhass <ganzhass@nornik.ru>
2024-06-20 06:48:50 +0300

    5.1DOM doc loading. Start

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,19 @@
 export default class NotificationMessage {
+  static currVisibleComponent;
+  constructor(message, { duration = 2000, type = "success" }) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+  }
 
+  show() {
+    if (this.type === "sucess") this.type = "error";
+    if (NotificationMessage.currVisibleComponent) {
+      NotificationMessage.currVisibleComponent.hide();
+    }
+    NotificationMessage.currVisibleComponent = this;
+    document.body.append(this);
+  }
+
+  hide() {}
 }
