b0065020c7fa73e2fe3190a63a5408b9e10c4846
ganzhass <ganzhass@nornik.ru>
2024-06-24 10:07:55 +0300

    5.1DOM doc loading. Errors 2

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,19 +1,60 @@
 export default class NotificationMessage {
   static currVisibleComponent;
-  constructor(message, { duration = 2000, type = "success" }) {
+
+  constructor(message = "", { duration = 2000, type = "success" }) {
     this.message = message;
     this.duration = duration;
     this.type = type;
+
+    this.element = this.createElement();
+    // console.log("element", this.element.outerHTML);
   }
 
-  show() {
-    if (this.type === "sucess") this.type = "error";
+  createElement() {
+    const element = document.createElement(`div`);
+    element.setAttribute("class", "notification inner-wrapper");
+    element.innerHTML = `
+    <div class = "notification ${this.type}">
+      <div class = 'timer' style="--value: ${this.t}"><div style="--value: 50"></div></div>
+      <div class="notification-header">Notification</div>
+      <div class="notification-body">${this.message}</div>}
+    </div>
+    `;
+    // console.log("element.outer", element.outerHTML);
+    // console.log("element.firstElementChild", element.firstElementChild);
+    return element.firstElementChild;
+  }
+
+  show(container = document.body) {
+    console.log("this.element.className", this.element.className);
+
     if (NotificationMessage.currVisibleComponent) {
-      NotificationMessage.currVisibleComponent.hide();
+      NotificationMessage.currVisibleComponent.destroy();
     }
     NotificationMessage.currVisibleComponent = this;
-    document.body.append(this);
-  }
+    // console.log(this.duration);
+    let dt = new Date().getTime();
+    this.t = setInterval(function () {
+      // console.log(
+      //   "показываем в консоли сколько прошлo = ",
+      // this.duration
+      // (this.duration - new Date().getTime() - dt)
+      // );
+    }, this.duration / 10);
 
+    this.timerID = setTimeout(() => {
+      this.destroy();
+    }, this.duration);
+    container.appendChild(this.element);
+  }
+  remove() {
+    this.element.remove();
+  }
   hide() {}
+
+  destroy() {
+    clearTimeout(this.timerID);
+    clearInterval(this.t);
+    this.remove();
+  }
 }
