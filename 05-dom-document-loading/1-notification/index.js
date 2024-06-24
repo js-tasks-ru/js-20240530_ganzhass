cb502e36c5716337db5f5354f150a286eb97e624
ganzhass <ganzhass@nornik.ru>
2024-06-25 00:05:47 +0300

    5.1 error

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -5,42 +5,40 @@ export default class NotificationMessage {
     this.message = message;
     this.duration = duration;
     this.type = type;
-
     this.element = this.createElement();
-    // console.log("element", this.element.outerHTML);
   }
 
   createElement() {
     const element = document.createElement(`div`);
-    element.setAttribute("class", "notification inner-wrapper");
+
     element.innerHTML = `
-    <div class = "notification ${this.type}">
-      <div class = 'timer' style="--value: ${this.t}"><div style="--value: 50"></div></div>
-      <div class="notification-header">Notification</div>
-      <div class="notification-body">${this.message}</div>}
+    <div class = "notification ${this.type}  inner-wrapper" style="--value: 30">
+      <div class = "timer" style="--value: ${this.t}}"></div>
+      <div class = "notification-header">Notification</div>
+      <div class = "notification-body">${this.message}</div>
     </div>
     `;
-    // console.log("element.outer", element.outerHTML);
-    // console.log("element.firstElementChild", element.firstElementChild);
+
     return element.firstElementChild;
   }
 
   show(container = document.body) {
-    console.log("this.element.className", this.element.className);
-
     if (NotificationMessage.currVisibleComponent) {
       NotificationMessage.currVisibleComponent.destroy();
     }
     NotificationMessage.currVisibleComponent = this;
-    // console.log(this.duration);
+
     let dt = new Date().getTime();
     this.t = setInterval(function () {
+      // NOTE: почему:
+      // duration = underfined
+      // console.log(this.duration);
       // console.log(
       //   "показываем в консоли сколько прошлo = ",
-      // this.duration
-      // (this.duration - new Date().getTime() - dt)
+      //   this.duration - new Date().getTime() - dt
       // );
     }, this.duration / 10);
+    console.log(this.t);
 
     this.timerID = setTimeout(() => {
       this.destroy();
